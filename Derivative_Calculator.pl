eef406de9b7acbb850f4701501edad52ab260bbf
Griger5 <griger04@gmail.com>
2024-02-21 21:56:05 +0100

    Added cyclometric and hyperbolic funtions, both base and exponent can now be functions of x

--- Derivative_Calculator.pl
+++ Derivative_Calculator.pl
@@ -10,6 +10,9 @@ derivative(F*G, Result) :- derivative(F, DerF), derivative(G, DerG), Result = De
 derivative(F/G, Result) :- derivative(F, DerF), derivative(G, DerG), Result = (DerF*G - F*DerG)/G^2.
 derivative(_^0, 0).
 derivative(X^N, Result) :- number(N), N\=0, NewExp is N-1, delexponent0(X^NewExp, Xexp), derivative(X, Result2), Result = N*Xexp*Result2.
+derivative(F^G, Result) :- \+number(F), \+number(G), derivative(F, DerF), derivative(G, DerG), NewExp = G-1, Result = G*DerF*F^NewExp + DerG*ln(F)*F^G.
+derivative(X^(N/M), Result) :- number(N), number(M), N\=0, M\=0, NewExp is N/M-1, delexponent0(X^NewExp, Xexp), derivative(X, Result2), Result = N/M*Xexp*Result2.
+derivative(sqrt(X), Result) :- derivative(X^0.5, Result).
 
 % trigonometric functions
 derivative(sin(X), Result) :- derivative(X, Result2), Result = cos(X)*Result2.
@@ -25,3 +28,15 @@ derivative(log(N, X), Result) :- number(N), N\=e, derivative(X, Result2), Result
 % exponential functions
 derivative(e^X, Result) :- derivative(X, Result2), Result = e^X*Result2.
 derivative(N^X, Result) :- number(N), N\=0, N\=e, derivative(X, Result2), Result = N^X*ln(N)*Result2.
+
+% cyclometric functions
+derivative(arcsin(X), Result) :- derivative(X, Result2), Result = Result2/sqrt(1-X^2).
+derivative(arccos(X), Result) :- derivative(X, Result2), Result = -Result2/sqrt(1-X^2).
+derivative(arctg(X), Result) :- derivative(X, Result2), Result = Result2/(1+X^2).
+derivative(arcctg(X), Result) :- derivative(X, Result2), Result = -Result2/(1+X^2).
+
+% hyperbolic functions
+derivative(sinh(X), Result) :- derivative(X, Result2), Result = cosh(X)*Result2.
+derivative(cosh(X), Result) :- derivative(X, Result2), Result = sinh(X)*Result2.
+derivative(tgh(X), Result) :- derivative(X, Result2), Result = Result2/(cosh(X))^2.
+derivative(ctgh(X), Result) :- derivative(X, Result2), Result = -Result2/(sinh(X))^2.
