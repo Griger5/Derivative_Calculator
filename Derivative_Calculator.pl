a4c0dd73d830ef82362f9fbecaa9d7045fed264c
Griger5 <griger04@gmail.com>
2024-02-21 11:11:06 +0100

    Added product rule, quotient rule, tangent and cotangent

--- Derivative_Calculator.pl
+++ Derivative_Calculator.pl
@@ -1,10 +1,18 @@
 delexponent0(_^0, 1).
-delexponent0(X^N, X^N).
+delexponent0(X^1, X).
+delexponent0(X^N, Result) :- N\=1, Result = X^N.
 
 % basic functions
-derivative(N*X, Result) :- derivative(X, Result2), Result = N*Result2.
-derivative(X^N, Result) :- NewExp is N-1, delexponent0(X^NewExp, Result2), Result = N*Result2.
+derivative(N, Result) :- number(N), Result is 0.
+derivative(x, 1).
+derivative(F+G, Result) :- derivative(F, DerF), derivative(G, DerG), Result = DerF + DerG.
+derivative(F*G, Result) :- derivative(F, DerF), derivative(G, DerG), Result = DerF*G + F*DerG.
+derivative(F/G, Result) :- derivative(F, DerF), derivative(G, DerG), Result = (DerF*G - F*DerG)/G^2.
+derivative(_^0, 0).
+derivative(X^N, Result) :- N\=0, NewExp is N-1, delexponent0(X^NewExp, Result2), Result = N*Result2.
 
 % trigonometric functions
 derivative(sin(X), Result) :- derivative(X, Result2), Result = cos(X)*Result2.
 derivative(cos(X), Result) :- derivative(X, Result2), Result = -sin(X)*Result2.
+derivative(tg(X), Result) :- derivative(X, Result2), Result = Result2/(cos(X))^2.
+derivative(ctg(X), Result) :- derivative(X, Result2), Result = -Result2/(sin(X))^2.
