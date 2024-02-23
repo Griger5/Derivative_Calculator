0f63b001684e6a129a0464c351e0271bb0d13313
Griger5 <griger04@gmail.com>
2024-02-23 23:54:49 +0100

    Added main function for easy use, fixed F^G derivative

--- Derivative_Calculator.pl
+++ Derivative_Calculator.pl
@@ -4,15 +4,11 @@ delexponent0(X^N, Result) :- N\=1, Result = X^N.
 
 % basic functions
 derivative(N, Result) :- number(N), Result = 0.
+derivative(N/M, Result) :- number(N), number(M), Result = 0.
 derivative(x, 1).
 derivative(F+G, Result) :- derivative(F, DerF), derivative(G, DerG), Result = DerF + DerG.
 derivative(F*G, Result) :- derivative(F, DerF), derivative(G, DerG), Result = DerF*G + F*DerG.
-derivative(F/G, Result) :- derivative(F, DerF), derivative(G, DerG), Result = (DerF*G - F*DerG)/G^2.
-derivative(_^0, 0).
-derivative(X^N, Result) :- number(N), N\=0, NewExp is N-1, delexponent0(X^NewExp, Xexp), derivative(X, Result2), Result = N*Xexp*Result2.
-derivative(F^G, Result) :- \+number(F), \+number(G), derivative(F, DerF), derivative(G, DerG), NewExp = G-1, Result = G*DerF*F^NewExp + DerG*ln(F)*F^G.
-derivative(X^(N/M), Result) :- number(N), number(M), N\=0, M\=0, NewExp is N/M-1, delexponent0(X^NewExp, Xexp), derivative(X, Result2), Result = N/M*Xexp*Result2.
-derivative(sqrt(X), Result) :- derivative(X^0.5, Result).
+derivative(F/G, Result) :- \+number(F), \+number(G), derivative(F, DerF), derivative(G, DerG), Result = (DerF*G - F*DerG)/G^2.
 
 % trigonometric functions
 derivative(sin(X), Result) :- derivative(X, Result2), Result = cos(X)*Result2.
@@ -22,10 +18,16 @@ derivative(ctg(X), Result) :- derivative(X, Result2), Result = -Result2/(sin(X))
 
 % logarithmic functions
 derivative(ln(X), Result) :- derivative(X, Result2), Result = Result2/X.
-derivative(log(e, X), Result) :- derivative(X, Result2), Result = Result2/X.
+derivative(log(e, X), Result) :- derivative(ln(X), Result).
 derivative(log(N, X), Result) :- number(N), N\=e, derivative(X, Result2), Result = Result2/(X*ln(N)).
 
 % exponential functions
+derivative(_^0, 0).
+derivative(X^N, Result) :- number(N), N\=0, NewExp is N-1, delexponent0(X^NewExp, Xexp), derivative(X, Result2), Result = N*Xexp*Result2.
+derivative(F^G, Result) :- \+number(F), \+number(G), G\=_/_, derivative(F, DerF), derivative(G, DerG), NewExp = G-1, Result = G*DerF*F^NewExp + DerG*ln(F)*F^G.
+derivative(F^G, Result) :- \+number(F), \+number(G), G=N/M, \+number(N), \+number(M), derivative(F, DerF), derivative(G, DerG), NewExp = G-1, Result = G*DerF*F^NewExp + DerG*ln(F)*F^G.
+derivative(X^(N/M), Result) :- number(N), number(M), N\=0, M\=0, NewExp is N/M-1, delexponent0(X^NewExp, Xexp), derivative(X, Result2), Result = N/M*Xexp*Result2.
+derivative(sqrt(X), Result) :- derivative(X^0.5, Result).
 derivative(e^X, Result) :- derivative(X, Result2), Result = e^X*Result2.
 derivative(N^X, Result) :- number(N), N\=0, N\=e, derivative(X, Result2), Result = N^X*ln(N)*Result2.
 
@@ -40,3 +42,11 @@ derivative(sinh(X), Result) :- derivative(X, Result2), Result = cosh(X)*Result2.
 derivative(cosh(X), Result) :- derivative(X, Result2), Result = sinh(X)*Result2.
 derivative(tgh(X), Result) :- derivative(X, Result2), Result = Result2/(cosh(X))^2.
 derivative(ctgh(X), Result) :- derivative(X, Result2), Result = -Result2/(sinh(X))^2.
+
+
+main :-
+    write("Enter the function that you want to calculate the derivative of:"),
+	read(Function),
+	derivative(Function, Result),
+    writeln(Result),
+    fail.
